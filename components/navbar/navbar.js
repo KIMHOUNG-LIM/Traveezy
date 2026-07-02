3eb7a4003e9669728b455cf8a4801d0a01a59d72
KIMHOUNG-LIM <kimhounglim.k@gmail.com>
2026-07-02 22:57:46 +0700

    add navbar to home page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,1 +1,8 @@
 // navbar component logic
+async function loadNavbar() {
+    const response = await fetch("components/navbar/navbar.html");
+    const html = await response.text();
+    document.getElementById("navbar-container").innerHTML = html;
+}
+
+document.addEventListener("DOMContentLoaded", loadNavbar);
