fe6fdb961a85853a51ca930f47324ffdbce21e86
KIMHOUNG-LIM <kimhounglim.k@gmail.com>
2026-07-05 22:53:04 +0700

    add second section, edite the hero swiper

--- features/home/home.js
+++ features/home/home.js
@@ -1,10 +1,31 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 2,
+  slidesPerView: 3,
+  spaceBetween: 24,
   loop: true,
   speed: 1000,           // how fast the slide-in motion happens (ms)
+  centeredSlides: false,
 
   autoplay: {
     delay: 2000,
     disableOnInteraction: false,
+    reverseDirection: true, // Make autoplay scroll to the right
   },
+
+  breakpoints: {
+    // When window width is >= 320px (mobile)
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 16
+    },
+    // When window width is >= 768px (tablet)
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // When window width is >= 1024px (desktop)
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24
+    }
+  }
 });
