ea43f40bd53a0b9fc3f7fd78a3856ed255b23dd2
Mara <yenmara200@gmail.com>
2026-07-13 15:25:45 +0700

    change home js

--- features/home/home.js
+++ features/home/home.js
@@ -1,10 +1,26 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 2,
+  slidesPerView: 3,
+  spaceBetween: 24,
   loop: true,
   speed: 1000,           // how fast the slide-in motion happens (ms)
+  centeredSlides: false,
 
   autoplay: {
     delay: 2000,
     disableOnInteraction: false,
+    reverseDirection: true, // Make autoplay scroll to the right
   },
+
+  breakpoints: {
+    // When window width is >= 320px (mobile / tablet)
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 8
+    },
+    // When window width is >= 1024px (desktop)
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24
+    }
+  }
 });
