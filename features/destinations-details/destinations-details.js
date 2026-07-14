f1a13645ad32ee0f9b4460963d83616c1a4dbc91
unknown <tepny8042006@gmail.com>
2026-07-14 23:04:50 +0700

    destination

--- features/destinations-details/destinations-details.js
+++ features/destinations-details/destinations-details.js
@@ -1,1 +1,25 @@
 // destinations feature logic
+const slider = document.querySelector(".food-scroll");
+
+if (slider) {
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener("mousedown", (e) => {
+        isDown = true;
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener("mouseleave", () => isDown = false);
+    slider.addEventListener("mouseup", () => isDown = false);
+
+    slider.addEventListener("mousemove", (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2; // Drag speed
+        slider.scrollLeft = scrollLeft - walk;
+    });
+}
